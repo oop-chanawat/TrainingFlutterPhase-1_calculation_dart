b64a2b2a984de3284ede836494cad87933bc9fb5
oop-chanawat <54656470+oop-chanawat@users.noreply.github.com>
2022-09-09 17:26:17 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Professnow',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  double firstnum = 0;
+  double secondnum = 0;
+  String texttodisplay = '';
+  String res = '';
+  String operatortoperform = '';
+  TextEditingController input_num = TextEditingController();
+  void btnclicked(String btnvalue) {
+    if (btnvalue == '+') {
+      firstnum += double.parse(input_num.text)
+    }
+
+    setState(() {
+
+    });
+  }
+
+  // button class start
+  Widget custombutton(String btnvalue) {
+    return Expanded(
+      child: OutlineButton(
+        padding: EdgeInsets.all(20),
+        onPressed: () => btnclicked(btnvalue),
+        child: Text(
+          '$btnvalue',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+// button class end
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Expanded(
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    '$texttodisplay',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ),
+
+            Expanded(child: TextFormField(
+              controller: input_num,
+            )),
+
+            Expanded(child: Row( children: <Widget>[
+              custombutton('+'),
+              custombutton('-'),
+              custombutton('x'),
+              custombutton('/'),
+            ],
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
